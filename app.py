1672deeeda82d14972e950e0653a7b5996f1d7d9
Oluwatosin Olutayo Oguntola <tosinable19@gmail.com>
2025-08-25 11:48:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,4 @@ def home():
     return "Hello from Python Flask on AWS EC2 via GitHub Actions!"
 
 if __name__ == "__main__":
-    # Runs on port 5000 by default
     app.run(host="0.0.0.0", port=5000)
